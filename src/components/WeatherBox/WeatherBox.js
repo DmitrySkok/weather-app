5ad7d550ead873f43ce4c837137741fb55920e8c
Dmitry Skok <dimas.skok95@gmail.com>
2022-03-29 22:49:12 +0200

    Added getting data from API and output to console

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -4,8 +4,19 @@ import Loader from '../Loader/Loader';
 import { useCallback } from 'react';
 
 const WeatherBox = props => {
+  
   const handleCityChange = useCallback(cityName => {
-      console.log(cityName);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=2bb5d7a9d37784827dcf08c3dc8acae0&units=metric`)
+    .then(res => res.json())
+    .then(data => {
+      const weatherData = {
+        city: data.name,
+        temp: data.main.temp,
+        icon: data.weather[0].icon,
+        description: data.weather[0].main
+      };
+      console.log(weatherData);
+    });
     },[]);
   
 
